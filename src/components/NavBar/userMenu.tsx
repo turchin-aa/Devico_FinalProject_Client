822aac61476256f3620355540a2c2a2564d0f901
Nika Zelenska <zelenskaya.nika@gmail.com>
2022-05-13 16:48:14 +0300

    fixed some things

--- src/components/NavBar/userMenu.tsx
+++ src/components/NavBar/userMenu.tsx
@@ -1,49 +1,39 @@
 import { MenuItem } from '@mui/material'
-import { useState, MouseEvent, useCallback } from 'react'
+import { useCallback } from 'react'
 import { memo } from 'react'
-import { RootState } from '../../store/index'
-import { userSliceActions } from '../../store/user-slice'
 import { useDispatch, useSelector } from 'react-redux'
-import { menuOut, menuIn } from './userMenuVar'
 import useStyles from '../../theme/useStyle'
+import { uiActions } from '../../store/ui-slice'
 
 const UserMenu = () => {
   const classes = useStyles()
 
-  const isUserAuth = useSelector<RootState, boolean>(state => state.user.isAuth)
   const dispatch = useDispatch()
 
-  const [menu, setMenu] = useState(isUserAuth ? menuIn : menuOut)
+  const toggleRegHandler = useCallback(() => {
+    dispatch(uiActions.toggleReg())
+  }, [dispatch])
 
-  const handleClick = useCallback((e: MouseEvent<HTMLButtonElement>) => {
-    const target = e.currentTarget as HTMLButtonElement
-    switch (target.name) {
-      case 'Sign Out':
-        dispatch(userSliceActions.toggleAuth())
-        if (isUserAuth) {
-          setMenu(menuOut)
-        } else {
-          setMenu(menuIn)
-        }
-        break
-      default:
-        break
-    }
-  }, [])
+  const toggleLogHandler = useCallback(() => {
+    dispatch(uiActions.toggleLog())
+  }, [dispatch])
 
   return (
     <div>
-      {menu.map(item => (
-        <MenuItem
-          component="button"
-          key={item.name}
-          className={classes.userBarDropdownButtons}
-          onClick={handleClick}
-          name={item.name}
-        >
-          {item.name}
-        </MenuItem>
-      ))}
+      <MenuItem
+        component="button"
+        className={classes.userBarDropdownButtons}
+        onClick={toggleRegHandler}
+      >
+        Sign Up
+      </MenuItem>
+      <MenuItem
+        component="button"
+        className={classes.userBarDropdownButtons}
+        onClick={toggleLogHandler}
+      >
+        Sign In
+      </MenuItem>
     </div>
   )
 }
--- src/components/home-page/welcome/WelcomeTextBlock.tsx
+++ src/components/home-page/welcome/WelcomeTextBlock.tsx
@@ -1,12 +1,11 @@
-<<<<<<< HEAD:src/components/HomePage/WelcomeTextBlock.tsx
 import { Button, Typography } from '@mui/material'
-import useStyles from '../styles/useStyle'
 import { useDispatch } from 'react-redux'
-import { uiActions } from '../../store/ui-slice'
+import { uiActions } from '../../../store/ui-slice'
 import { useCallback } from 'react'
+import useWelcomeStyles from './useWelcomeStyles'
 
 const WelcomeText = () => {
-  const classes = useStyles()
+  const classes = useWelcomeStyles()
 
   const dispatch = useDispatch()
 
@@ -21,27 +20,6 @@ const WelcomeText = () => {
   const showRecoverPasHandler = useCallback(() => {
     dispatch(uiActions.toggleForgetPassword())
   }, [dispatch])
-=======
-import { Button } from '@mui/material'
-import useWelcomeStyles from './useWelcomeStyles'
-import { useNavigate } from 'react-router-dom'
-
-const WelcomeText = () => {
-  const classes = useWelcomeStyles()
-  const navigate = useNavigate()
-  const handleClick = (name: string) => {
-    switch (name) {
-      case 'Sign in':
-        navigate('/signin')
-        break
-      case 'Sign up':
-        navigate('/signup')
-        break
-      default:
-        break
-    }
-  }
->>>>>>> znika-fix-responsive-design:src/components/home-page/welcome/WelcomeTextBlock.tsx
 
   return (
     <div className={classes.welcomeBlock}>
